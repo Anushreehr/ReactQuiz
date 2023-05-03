1518c75a79ce0c2364e08629911bc406a2f545de
unknown <anushreehr791@gmail.com>
2023-05-03 23:05:53 +0530

    Quiz

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Screen1 from "./components/Screen1";
 import Screen2 from "./components/Screen2";
 import QuizApp from "./components/Screen2";
-import last_page from "./components/Screen3";
+
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Screen1 />} />
         <Route path='/screen2/2' element={<QuizApp />} />
-        <Route path='/screen3' element={<last_page />} />
+       
       </Routes>
     </BrowserRouter>
   );
